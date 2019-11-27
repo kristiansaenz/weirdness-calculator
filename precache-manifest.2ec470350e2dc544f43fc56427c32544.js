dc9cae20d4bce92aaaa6f7412f38ba8544c89a44
Your Name <you@example.com>
2019-11-27 16:28:17 -0600

    Updates

---
+++ precache-manifest.2ec470350e2dc544f43fc56427c32544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f489817e1683b09b2a9c13fe3a589",
+    "url": "/weirdness-calculator/index.html"
+  },
+  {
+    "revision": "db1b8fef8cf81239a54d",
+    "url": "/weirdness-calculator/static/css/main.b9d61666.chunk.css"
+  },
+  {
+    "revision": "49200fb05fb431340d7a",
+    "url": "/weirdness-calculator/static/js/2.89a132fd.chunk.js"
+  },
+  {
+    "revision": "db1b8fef8cf81239a54d",
+    "url": "/weirdness-calculator/static/js/main.72d88d66.chunk.js"
+  },
+  {
+    "revision": "1908a4d47b2c0488af98",
+    "url": "/weirdness-calculator/static/js/runtime-main.5190e9e8.js"
+  },
+  {
+    "revision": "92f39cf3488609bda55bccf87f16d17b",
+    "url": "/weirdness-calculator/static/media/thumbs-up.92f39cf3.svg"
+  }
+]);

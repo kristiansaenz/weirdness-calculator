c98e98c8f30364a08889cf1ac7d45236a26bd377
Your Name <you@example.com>
2019-11-29 12:54:21 -0600

    feature: creating gh-page and fixing section margins

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import App from "./App";
 import ScoreResultsContainer from "./containers/ScoreResultsContainer";
 import * as serviceWorker from "./serviceWorker";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 
+const basename = process.env.PUBLIC_URL;
+
 const routing = (
-  <Router>
+  <BrowserRouter basename={basename}>
     <div>
       <Header />
       <Route exact path="/" component={App} />
       <Route path="/results" component={ScoreResultsContainer} />
       <Footer />
     </div>
-  </Router>
+  </BrowserRouter>
 );
 
 ReactDOM.render(routing, document.getElementById("root"));

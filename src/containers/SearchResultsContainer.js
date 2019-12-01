db61903a5980e47ac9b7b7d6e5bee5bb10d4f6df
Your Name <you@example.com>
2019-12-01 01:00:29 -0600

    feature: adding api fetch to search component

--- src/containers/SearchResultsContainer.js
+++ src/containers/SearchResultsContainer.js
@@ -1,10 +1,26 @@
-import React from "react";
-import SearchResults from "../components/SearchResults"
+import React, { useState, useEffect } from "react";
+import SearchResults from "../components/SearchResults";
+import axios from "axios";
 
 function SearchResultsContainer() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  const GIF_URL = "api.giphy.com/v1/gifs/translate";
+  const API_KEY = "iuWW7RqfUxuHRrD6H33ZzFFmZTFLnFT9";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const gif = await axios.get(
+        "http://api.giphy.com/v1/gifs/translate?api_key=iuWW7RqfUxuHRrD6H33ZzFFmZTFLnFT9&s=otter&weirdness=6"
+      );
+      setSearchResult(gif.data.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
-        <SearchResults />
+      <SearchResults gif={searchResult} />
     </div>
   );
 }
